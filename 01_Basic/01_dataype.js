caf8cbe1e236e099f297daaed8be1089091d803a
Sneha Debnath <137872479+Rai21@users.noreply.github.com>
2025-06-13 05:36:08 +0000

    Basics

---
+++ 01_Basic/01_dataype.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using, not browser
+
+console.log(3+3); // code readibility should be high. do not do this 
+
+console.log("Rai");
+
