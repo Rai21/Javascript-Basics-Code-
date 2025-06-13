2deabf894069a4eaaeacc4aa6a572cfe81534099
Sneha Debnath <137872479+Rai21@users.noreply.github.com>
2025-06-13 05:48:46 +0000

    datatype

--- 01_Basic/01_dataype.js
+++ 01_Basic/01_dataype.js
@@ -6,3 +6,22 @@ console.log(3+3); // code readibility should be high. do not do this
 
 console.log("Rai");
 
+let name ="Rai"
+let age = 23
+let address ="konnagar"
+let state;
+// PREMETIVES
+// number => 2 to power 53
+// bigint => it is used for big number
+// String => ""
+// boolean => true / false
+// null => standalone value or empty
+// undefined => 
+// symbol => for find uniqueness
+
+//
+// object
+
+console.log(typeof undefined);  // output : undefined
+console.log(typeof null);  // output: object
+
