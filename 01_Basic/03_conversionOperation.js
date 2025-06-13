57887d4067bdfcc22ee6aa96502b67c400f4bb83
Sneha Debnath <137872479+Rai21@users.noreply.github.com>
2025-06-13 06:14:21 +0000

    operations

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -10,17 +10,34 @@ console.log(valueInNumber);
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
-
+/*
 let isLoggedIn = "Rai"
 
 let booleanLoogedIn =Boolean(isLoggedIn)
 console.log(booleanLoogedIn);
-
+*/
 // 1 => true ; 0 => false
 // "" => false
 // "rai" => true
 
+/*
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// ***************OPERATOR****************
+
+// let value =3
+// let negvalue = -value
+// console.log(negvalue);
+
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
