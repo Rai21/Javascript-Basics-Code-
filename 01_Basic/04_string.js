b333ef0b7e03a604dc78e4000e9a901272bf172f
Sneha Debnath <137872479+Rai21@users.noreply.github.com>
2025-06-14 05:58:23 +0000

    split

--- 01_Basic/04_string.js
+++ 01_Basic/04_string.js
@@ -2,7 +2,8 @@ const name="Rai" // 1st way to assign String
 const age=23
 console.log(`Hello my name is ${name} and my age is ${age}`)// string interpolletion
 
-const gameName = new String('rai-debn') // 2nd way to assign String
+
+const gameName = new String('rai-debn-cm') // 2nd way to assign String
 console.log(gameName[1]); // access key-value pair in object
 console.log(gameName.__proto__);//output:{} //its assign prototype
 
@@ -15,13 +16,19 @@ console.log(gameName.indexOf('d')); // to know which ccharacter  are placed on w
 
 const newString = gameName.substring(0,4)
 console.log(newString);
-
 const anotherString = gameName.slice(-8, 4) // slicing
 console.log(anotherString);
 
+
 const newStringOne = "  Rai  "
 console.log(newStringOne);;
-console.log(newStringOne.trim());
+console.log(newStringOne.trim()); // trim() is a function and it is used for cut the blank/white space .it has two types 1.trim start and trim stop
 
 
+const url = "https://rai.com/Sneha%20debnath"
+console.log(url.replace('%20','-')) // in url replace anythinf by using replace() function
+console.log(url.includes('Guddu')); // in url if any name or keyword is present or not for checking this used includes(). output false
+console.log(url.includes('Sneha')); // output: true
 
+
+console.log(gameName.split('-')); // split the gamename by '-' separator
