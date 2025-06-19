a6f6c5456affa4b04b46ad66c85097a68cbb0327
Sneha Debnath <137872479+Rai21@users.noreply.github.com>
2025-06-19 15:23:44 +0000

    function

---
+++ 03_Basic/01_function.js
@@ -0,0 +1,44 @@
+function sayMyName(){  
+    console.log("S");
+    console.log("N");
+    console.log("E");
+    console.log("H");
+    console.log("A");
+}
+
+// sayMyName()    //sayMyName = reference, () = execution
+/*
+function addTwoNumbers(number1, number2) { // (number1, number2) = parameters
+    console.log(number1+number2);
+}
+addTwoNumbers(3,null) // (3,null) = Arguments
+*/
+
+function addTwoNumbers(number1, number2) { // (number1, number2) = parameters
+    //let result = number1+number2
+    //return result  
+    //console.log("rai"); // after return statement console can not work
+    return number1+number2 // can add in one line under return statement
+    
+}
+
+const result = addTwoNumbers(3,4)
+//console.log("Result: ", result);
+
+
+function loginUserMessage(username ="sam"){ // if no one visit then(username="sam") will be print
+    if (username === undefined){ // undefined means false value we can write it like (!username)
+        console.log("please enter a username")
+        return
+    }
+    return `${username} just loggged in`
+}
+// console.log(loginUserMessage("RAi"));
+// console.log(loginUserMessage());
+
+function calculateCarPrice(...num1){ // ... is rest operator also.
+    return num1
+}
+//console.log(calculateCarPrice(200,400,500,2000));
+
+
