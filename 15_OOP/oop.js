2a6cc0dfde9398d9f176878c3d13f4d33d3b50c1
Sneha Debnath <137872479+Rai21@users.noreply.github.com>
2025-07-01 17:33:03 +0530

    oop

--- 15_OOP/oop.js
+++ 15_OOP/oop.js
@@ -1,3 +1,5 @@
+// 1.this keyword
+
 const user ={
     username : "Rai",
     loginCount : 8,
@@ -14,7 +16,37 @@ const user ={
     }
 
 }
-console.log(user.username);
+// console.log(user.username);
 // console.log(user.getUserDetails());
-console.log(this);
+// console.log(this);
 
+// 2.Constructor function
+
+//const promisOne = new Promise()
+//const date = new Date() // new is used for Constructor function
+
+function User(username, logicCount, isLoggedIn){
+    this.username = username
+//      |               |
+//     V               V 
+//    VARIABLE      value for pass
+    this.logicCount= logicCount
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome${this.username}`);
+        
+    }
+    return this // by default implicitly return this
+} // .this() is actually object
+
+// const userOne =  User("Rai",12,true)
+// const userTwo =  User("Rai",11,false)
+
+// console.log(userOne); // it will overWrite
+const userOne = new User("Rai",12,true)
+const userTwo = new User("Rai",11,false)
+console.log(userOne.constructor); // [Function: User]
+
+console.log(userOne);
+// new keyword genarate a empty keywordd {} called new object and called Instance
